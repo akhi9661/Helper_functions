d3d72012f624a9565dc9abc6d7a3acdc21507091
Akhilesh Kumar <63473666+akhi9661@users.noreply.github.com>
2023-07-02 23:10:46 +0530

    Update extract_multi_points.py

--- extract_multi_points.py
+++ extract_multi_points.py
@@ -1,38 +1,37 @@
-import os, shutil, pandas as pd, rioxarray as rxr, geopandas as gpd, earthpy.plot as ep, rasterstats as rs
-from pylab import *
+import os
+import shutil
+import pandas as pd
+import geopandas as gpd
+import rasterio
+from rasterio.mask import mask
 
 def extract_sr(rast_path, rast_name, shp_filename, out_df):
-    
     print('Processing:', rast_name)
     src_filename = os.path.join(rast_path, rast_name)
-    src_rast = rxr.open_rasterio(src_filename, masked=True).squeeze()
-    cc = src_rast.rio.crs.to_proj4()
-    crs = cc.split('=')[1]
-    
-    pts = gpd.read_file(shp_filename)
-    pts_crs = pts.to_crs(crs)
-    
-    output_path = os.path.join(rast_path, "Temp_shp")
-    
-    if not os.path.isdir(output_path):
-        os.mkdir(output_path)
-    
-    temp_shp = os.path.join(output_path, "temp_shp.shp")
-    pts_crs.to_file(temp_shp)
-    
-    col_name = rast_name.split('.')[0]
-    src_pixel = rs.zonal_stats(temp_shp, src_rast.values,
-                           affine = src_rast.rio.transform(),
-                           geojson_out = True,
-                           copy_properties = True,
-                           stats = "mean")
-    
-    temp_df = gpd.GeoDataFrame.from_features(src_pixel)
-    out_df[col_name] = temp_df['mean']
-    
-    return (src_rast, pts_crs, out_df, output_path)
 
-def main(rast_path, shp_filename, visual = False):
+    # Read the raster file
+    with rasterio.open(src_filename) as src:
+        # Read the shapefile
+        pts = gpd.read_file(shp_filename)
+        pts_crs = pts.to_crs(src.crs)
+
+        # Iterate over each row in the shapefile
+        for idx, row in pts_crs.iterrows():
+            # Get the geometry of the current row
+            geom = row.geometry
+            # Perform masking
+            shapes = [geom]
+            # Read the raster data within the shapefile bounds
+            out_image, out_transform = mask(src, shapes, crop=True)
+            # Calculate zonal statistics (mean)
+            stats = {"mean": np.mean(out_image)}
+            # Add zonal statistics to the output dataframe
+            col_name = rast_name.split(".")[0]
+            out_df.at[idx, col_name] = stats["mean"]
+
+    return out_df
+
+def main(rast_path, shp_filename):
     
     out_df = pd.DataFrame()
 
@@ -42,22 +41,9 @@ def main(rast_path, shp_filename, visual = False):
 
     out_df.to_excel(os.path.join(rast_path, 'extracted.xlsx'), header = True) 
 
-    if visual:
-      
-        fig, ax = plt.subplots(figsize = (10, 10))
-        ep.plot_bands(src_rast, extent = plotting_extent(src_rast, src_rast.rio.transform()),
-                      cmap = 'Greys',
-                      title = "Random Points for Comparison",
-                      scale = False,
-                      ax = ax)
-
-        pts_crs.plot(ax = ax, marker = 'o', markersize = 15, color = 'red')
-        ax.set_axis_off()
-        plt.show()
-
     if os.path.exists(output_path):
         shutil.rmtree(output_path)
         
 rast_path = input('Enter folder path containing raster files: \n')
 shp_filename = input('Enter the shapefile layer [path + filename.shp]: \n')
-main(rast_path, shp_filename)
+df = main(rast_path, shp_filename)
